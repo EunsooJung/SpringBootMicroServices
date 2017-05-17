653bc4cecda05f2f2acf3405cb00a071126ec0c3
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-05-17 11:49:48 -0700

    Finish Use Redis distributed data structures exmaple.

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
@@ -12,7 +12,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
-//@EnableCaching
+@EnableCaching
 public class MicroServicesWithSpringBootApplication {
 	
 	@Bean
