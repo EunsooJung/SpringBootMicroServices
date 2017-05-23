d79f82a29314f21b8f6aa799e997baef1d4b928c
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-05-23 13:05:47 -0700

    S7-L7.2 Spring Boot Logging Application initial Commit.

---
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/OperationsLoggingApplication.java
@@ -0,0 +1,17 @@
+/*
+ * 
+ */
+
+package com.ej.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsLoggingApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OperationsLoggingApplication.class, args);
+	}
+	
+}
--- MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/OperationsLoggingApplicationTests.java
+++ MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/OperationsLoggingApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroServicesWithSpringBootApplicationTests {
+public class OperationsLoggingApplicationTests {
 
 	@Test
 	public void contextLoads() {
