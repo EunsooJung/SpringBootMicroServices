28dc8987666907e47aca7f84b4744f52ce4c06e0
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-05-28 13:35:10 -0700

    Lecture 8.2 Discover and register services with Eureka and Consul

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
@@ -6,8 +6,10 @@ package com.ej.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroServicesWithSpringBootApplication {
 	
 	public static void main(String[] args) {
