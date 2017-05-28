77c517ada35c4ba91aa5c8f9de6b94fbfd56662a
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-05-28 16:11:12 -0700

    Lecture 8.3 Route on the client with Ribbon initial Commit

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
@@ -1,13 +1,16 @@
 /*
- * ref.: https://github.com/livelessons-spring/building-microservices/blob/master/livelessons-data/livelessons-data-jdbc/src/main/java/demo/DataJdbcApplication.java
  */
 
 package com.ej.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class MicroServicesWithSpringBootApplication {
 	
 	public static void main(String[] args) {
