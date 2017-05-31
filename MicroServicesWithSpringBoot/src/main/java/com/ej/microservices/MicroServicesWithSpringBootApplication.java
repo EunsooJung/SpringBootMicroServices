9384559396184baf7c2a1ccdfc5b94a3283e4147
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-05-31 14:44:56 -0700

    Added cloud annotations.

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
@@ -1,12 +1,19 @@
 /*
+ * 8.4 Prevent failure cascades using Hystrix
  */
 
 package com.ej.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
 public class MicroServicesWithSpringBootApplication {
 	
 	public static void main(String[] args) {
