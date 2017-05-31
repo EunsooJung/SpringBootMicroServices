c9c30d16c4c0d451692664c04f5c70e787016e49
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-05-31 14:34:41 -0700

    Lecture 8.4.1 Prevent failure cascades using Hystrix Dashboard Service.

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
@@ -1,13 +1,25 @@
 /*
+ * Section 8: Choreographing Microservices
+ *   Lecture 8.4.1 Prevent failure cascades using Hystrix Dashboard Service
  */
 
 package com.ej.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@EnableHystrixDashboard
 @SpringBootApplication
 public class MicroServicesWithSpringBootApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix/index.html";
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesWithSpringBootApplication.class, args);
