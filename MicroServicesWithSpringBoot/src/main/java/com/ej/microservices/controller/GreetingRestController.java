cb598ff1ce8d1425e1e990f4f438fe4c9dbdac7f
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-06-02 01:30:13 -0700

    Lecture 10.2 Understand HTTP Basic Version 2.

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/controller/GreetingRestController.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/controller/GreetingRestController.java
@@ -5,10 +5,11 @@ package com.ej.microservices.controller;
 
 import java.security.Principal;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,9 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingRestController {
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/hi")
-	public Map<String, String> greetings(Principal p) {
-		return Collections.singletonMap("content", "Hello, " + p.getName());
+	@RequestMapping("/hi")
+	public Map<String, Object> hi(Principal principal) {
+		Map<String, Object> result = new HashMap<>();
+		result.put("id", UUID.randomUUID().toString());
+		result.put("content","Hello, " + principal.getName());
+		return result;
 	}
 
 }
