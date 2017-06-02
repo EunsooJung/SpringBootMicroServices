bd720ae113dda64413a29ac9a20249aa6d26a43d
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-06-01 20:14:09 -0700

    Lecture 9.4 Resolve and transform resources And Section 9 Summary.

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
@@ -1,16 +1,53 @@
 /*
- * ref.: https://github.com/livelessons-spring/building-microservices/blob/master/livelessons-data/livelessons-data-jdbc/src/main/java/demo/DataJdbcApplication.java
+ * 
  */
 
 package com.ej.microservices;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.AppCacheManifestTransformer;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 
 @SpringBootApplication
-public class MicroServicesWithSpringBootApplication {
+public class MicroServicesWithSpringBootApplication extends WebMvcConfigurerAdapter {
+	
+	@Value("${application.version}")
+	private String version;
+	
+	@Value("${application.devmode}")
+	private boolean devmode;
+	
+	@Bean
+	public ResourceUrlEncodingFilter filter() {
+		return new ResourceUrlEncodingFilter();
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!this.devmode) {
+			registry.addResourceHandler("/**")
+			.addResourceLocations("classpath:/static/")
+			.resourceChain(true)
+			.addResolver(new VersionResourceResolver()
+					.addFixedVersionStrategy(this.version, "/**/*.js")
+					.addContentVersionStrategy("/**"))
+			.addTransformer(new AppCacheManifestTransformer());
+		}
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesWithSpringBootApplication.class, args);
