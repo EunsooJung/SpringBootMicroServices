76995d0d71a3dc36714463f2ef4a7897d5cecf2a
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-06-02 16:39:33 -0700

    Add @EnableResourceServer annotaion.

--- MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
+++ MicroServicesWithSpringBoot/src/main/java/com/ej/microservices/MicroServicesWithSpringBootApplication.java
@@ -7,8 +7,9 @@ package com.ej.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-
+@EnableResourceServer
 @SpringBootApplication
 public class MicroServicesWithSpringBootApplication {
 
