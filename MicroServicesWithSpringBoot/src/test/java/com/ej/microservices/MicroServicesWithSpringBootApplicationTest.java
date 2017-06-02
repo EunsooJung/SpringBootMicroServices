e0295b82cb8d1245652dfa699f9e2c4294da070d
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-06-01 22:33:19 -0700

    Lecture 10.1 Understand security initial commit.

---
+++ MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/MicroServicesWithSpringBootApplicationTest.java
@@ -0,0 +1,14 @@
+package com.ej.microservices;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MicroServicesWithSpringBootApplicationTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
