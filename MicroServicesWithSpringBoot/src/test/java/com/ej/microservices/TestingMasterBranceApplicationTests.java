a5c8f702183772c490bb56f47b633af804aca62b
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-06-04 15:15:49 -0700

    S11: Testing Microservices - L0 : Learning objectives initial commit.

--- MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/TestingMasterBranceApplicationTests.java
+++ MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/TestingMasterBranceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroServicesWithSpringBootApplicationTests {
+public class TestingMasterBranceApplicationTests {
 
 	@Test
 	public void contextLoads() {
