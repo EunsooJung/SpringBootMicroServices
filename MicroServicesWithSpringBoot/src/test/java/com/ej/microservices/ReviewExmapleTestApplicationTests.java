74966a95ce8dcd91f90fdba7b432d8ecdda4be87
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-06-04 15:25:44 -0700

    Lecture 11.1 Review the example test application intial commit.

--- MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/ReviewExmapleTestApplicationTests.java
+++ MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/ReviewExmapleTestApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestingMasterBranceApplicationTests {
+public class ReviewExmapleTestApplicationTests {
 
 	@Test
 	public void contextLoads() {
