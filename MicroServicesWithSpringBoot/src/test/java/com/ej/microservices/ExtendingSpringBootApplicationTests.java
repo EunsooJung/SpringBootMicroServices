b1c238a850401167c8189b3bc74a64fbd5946e74
Eunsoo Jung <hi.michael.jung@gmail.com>
2017-06-09 02:04:22 -0700

    Section12 master branch initial commit.

--- MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/ExtendingSpringBootApplicationTests.java
+++ MicroServicesWithSpringBoot/src/test/java/com/ej/microservices/ExtendingSpringBootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroServicesWithSpringBootApplicationTests {
+public class ExtendingSpringBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
